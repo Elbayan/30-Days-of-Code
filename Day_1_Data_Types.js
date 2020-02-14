42d2b4eee5eee5145d2bdd7c343b3f76f166a082
ElBayan <itaps02@gmail.com>
2020-02-14 16:59:15 +0500

    Day 1: Data Types

---
+++ Day_1_Data_Types.js
@@ -0,0 +1,33 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+// Reads complete line from STDIN
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+function main() {
+    var i = 4
+    var d = 4.0
+    var s = "HackerRank "
+    var l = Number(input_stdin_array[0]); 
+    var k = Number(input_stdin_array[1]);
+    var m = input_stdin_array[2];
+    
+ console.log (i + l);
+ console.log ( (d + k).toFixed(1) );
+ console.log (s + m);
+}
