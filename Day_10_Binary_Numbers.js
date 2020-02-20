5b99076dde0516e752e0af6da470fae374f8d4bf
ElBayan <itaps02@gmail.com>
2020-02-20 11:05:07 +0500

    Day 10: Binary Numbers

---
+++ Day_10_Binary_Numbers.js
@@ -0,0 +1,42 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+//beginning solution
+function main() {    
+    const binaryNumber = parseInt(readLine()).toString(2);   
+    const separatedNumbers = binaryNumber.split('');
+    let max = 0;
+    let current = 0;
+
+    for (let i = 0; i < separatedNumbers.length; i++) {
+
+      (separatedNumbers[i] == 1) ? current += 1 : current = 0;
+
+      (current > max) ? max = current : 0;  
+
+    }  
+    console.log(max);
+}
+//ending solution
